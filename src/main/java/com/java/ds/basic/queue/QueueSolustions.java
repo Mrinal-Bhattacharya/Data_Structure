a05458a7c0809c19bbef3ac71eb8985094f83545
BMRINAL <BMRINAL@WGC100C00QH32.Delhi.ford.com>
2016-06-06 14:49:30 +0530

    Signed-off-by: BMRINAL <BMRINAL@WGC100C00QH32.Delhi.ford.com>

--- src/main/java/com/java/ds/basic/queue/QueueSolustions.java
+++ src/main/java/com/java/ds/basic/queue/QueueSolustions.java
@@ -42,6 +42,103 @@ public class QueueSolustions {
 		return stack;
 	}
 
+	/*
+	 * Given a stack of integers, how do check whether each successive pair of
+	 * numbers in the stack is consecutive or not. The pairs can be increasing
+	 * or decreasing, and if the stack has an odd number of elements the element
+	 * at the top is left out of a pair. For example if the stack of elements
+	 * are [4,5,-2,-3,11,10,5,6,20], then the output should be true because each
+	 * of the pairs (4,5),(-2,-3), (11,10) and (5,6) consists of consecutive
+	 * numbers.
+	 */
+	public static boolean checkStackPairWiseOrder(final Stack<Integer> s) {
+		final Queue<Integer> q = new LinkedList<Integer>();
+		boolean pairWiseOrdered = true;
+		while (!s.empty()) {
+			q.add(s.pop());
+		}
+		while (!q.isEmpty()) {
+			s.push(q.remove());
+		}
+		while (!s.empty()) {
+			final int n = s.pop();
+			q.add(n);
+			if (!s.empty()) {
+				final int m = s.pop();
+				q.add(m);
+				if (Math.abs(n - m) != 1) {
+					pairWiseOrdered = false;
+				}
+			}
+		}
+		while (!q.isEmpty()) {
+			s.push(q.remove());
+		}
+		return pairWiseOrdered;
+	}
+
+	/*
+	 * Given a queue of integers, rearrange the elements by interleaving the
+	 * first half of the list with the second half of the list. For example,
+	 * suppose a queue stores the following sequence of
+	 * values:[11,12,13,14,15,16,17,18,19,20]. Consider the two halves of the
+	 * list: first half: [11,12,13,14,15] second half: [16,17,18,19,20]. These
+	 * are combined in an altering fashion to form a sequence of interleave
+	 * pairs: the first values from each half(11 and 16), then the second values
+	 * from each half (12 and 17), then the third values from each half(13 and
+	 * 18), and so on. In each pair, the value from the first half appears
+	 * before the value from the second half. Thus after the call, the queue
+	 * stores the following values:[11,16,12,17,13,18,14,19,15,20].
+	 */
+
+	public void interLeavingQueue(final Queue<Integer> q) {
+		if ((q.size() % 2) != 0) {
+			throw new IllegalArgumentException();
+		}
+		final Stack<Integer> s = new Stack<Integer>();
+		final int halfSize = q.size() / 2;
+		for (int i = 0; i < halfSize; i++) {
+			s.push(q.poll());
+		}
+		while (!s.isEmpty()) {
+			q.offer(s.pop());
+			for (int i = 0; i < halfSize; i++) {
+				q.offer(q.poll());
+			}
+		}
+		for (int i = 0; i < halfSize; i++) {
+			s.push(q.poll());
+		}
+		while (!s.isEmpty()) {
+			q.offer(s.pop());
+			q.offer(q.poll());
+		}
+	}
+
+	/*
+	 * Given an integer k and a queue of integers, how do you reverse the order
+	 * of the first k elements of the queue, leaving the other elements in the
+	 * same relative order? For example, if k=4 and queue has the elements
+	 * [10,20,30,40,50,60,70,80,90]; the output should be
+	 * [40,30,20,10,50,60,70,80,90].
+	 */
+	public static void reverseQueueFirstKElements(final int k, final Queue<Integer> q) {
+		if ((q == null) || (k > q.size())) {
+			throw new IllegalArgumentException();
+		} else if (k > 0) {
+			final Stack<Integer> s = new Stack<Integer>();
+			for (int i = 0; i < k; i++) {
+				s.push(q.remove());
+			}
+			while (!s.isEmpty()) {
+				q.add(s.pop());
+			}
+			for (int i = 0; i < (q.size() - k); i++) {
+				q.add(q.remove());
+			}
+		}
+	}
+
 	public static void main(final String[] args) {
 		final Queue<Integer> integers = new LinkedList<Integer>();
 		integers.add(1);
