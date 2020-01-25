9888ed6619ce212658aebe042cc51798e66cc490
Mrinal Bhattacharya <mrinal.bhattacharya@boeing.com>
2020-01-25 19:25:48 +0530

    gradle

--- src/main/java/com/interviewbit/binarytree/tries/Solutions.java
+++ src/main/java/com/interviewbit/binarytree/tries/Solutions.java
@@ -99,20 +99,3 @@ public class Solutions {
 		System.out.println("");
 	}
 }
-
-class TrieNode {
-	private final Map<Character, TrieNode> children = new HashMap<>();
-	private boolean endOfWord;
-
-	Map<Character, TrieNode> getChildren() {
-		return this.children;
-	}
-
-	boolean isEndOfWord() {
-		return this.endOfWord;
-	}
-
-	void setEndOfWord(final boolean endOfWord) {
-		this.endOfWord = endOfWord;
-	}
-}
